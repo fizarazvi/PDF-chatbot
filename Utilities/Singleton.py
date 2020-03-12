23a7339c910f2932d035d8656d41140533a6ff10
Ratansingh648 <ratansingh648@gmail.com>
2020-03-12 23:31:07 +0530

    Adding Singleton baseclass

---
+++ Utilities/Singleton.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 21 21:46:23 2020
+
+@author: Ratan Singh
+"""
+
+from Factory.DatabaseFactory import DatabaseFactory
+
+class Singleton(type):
+    
+    def __init__(cls, name, bases, attrs, **kwargs):
+        super().__init__(name, bases, attrs)
+        cls._instance = None
+        
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__call__(*args, **kwargs)
+        return cls._instance
+    
