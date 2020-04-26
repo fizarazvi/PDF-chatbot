9264b6eb43a1f5a2c9522fa03ef763e455fc9ee7
Srinath Ravikumar <srinathlrk@gmail.com>
2020-04-27 02:28:37 +0530

    Flow added in queryProcessor, supporting classes

---
+++ Processor/ElasticSearchToEmbeddings.py
@@ -0,0 +1,16 @@
+import Engines.QueryProcessor
+
+class ElasticSearchToEmbeddings():
+    def __init__(self):
+        pass
+
+    def ElasticSearchToEmbeddings(self, query, check):
+        if check == "query":
+            query_vec = 0
+
+            return query_vec
+
+        elif check == "data":
+            para_vec = 0
+
+            return para_vec
--- Processor/QuesDataToElasticSearch.py
+++ Processor/QuesDataToElasticSearch.py
@@ -5,3 +5,19 @@ Created on Fri Mar 13 19:35:04 2020
 @author: Srinath Ravikumar
 """
 
+import Engines.QueryProcessor
+
+class QuesDataToElasticSearch():
+
+    def __init__(self):
+        pass
+
+    def QuesDataToElasticSearch(self, raked_data, query_ranked_phrase_with_score):
+        selected_rake_para = 0
+
+        return selected_rake_para
+
+    def matchRakedParaToTextPara(self, selected_raked_para):
+        selected_text_para = 0
+
+        return selected_text_para
