7c5c3e78b33cb4f41de1b57f779a73235b4dd42c
riddhi927 <riddhi927@gmail.com>
2020-05-14 11:53:28 +0530

    changes in Elastic Server.py

--- ElasticServer.py
+++ ElasticServer.py
@@ -2,6 +2,7 @@ from elasticsearch import Elasticsearch
 import logging
 from datetime import datetime
 import pandas as pd
+import json
 
 from Utilities.Singleton import Singleton
 from ConfigParser.ConfigParser import ConfigurationParser
@@ -34,28 +35,30 @@ class ElasticServer:
             self.__logger.info("==" * 30)
             print(self.__es)
         except:
-            logging.error("[{}] : Could not connect to Elastic server.".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+            logging.error(
+                "[{}] : Could not connect to Elastic server.".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
             raise ConnectionError("Could not connect to Elastic server.")
 
-    def createIndex(self, index_name='pdfname'):
+    def createIndex(self, index_name):
         created = False
-        settings = {  # Not used 
+        settings = {  # Not used
             "settings": {
                 "number_of_shards": 1,  # change according to number of chunks
                 "number_of_replicas": 0
             },
             "mappings": {
-                "type_name": {
-                    "properties": {
-                        "Title": {
-                            "type": "text"
-                        },
-                        "Keywords": {
-                            "type": "text"
-                        },
-                        "Paragraph": {
-                            "type": "text"
-                        }
+                "properties": {
+                    "id": {
+                        "type": "integer"
+                    },
+                    "Title": {
+                        "type": "text"
+                    },
+                    "Keywords": {
+                        "type": "text"
+                    },
+                    "Paragraph": {
+                        "type": "text"
                     }
                 }
             }
@@ -63,7 +66,7 @@ class ElasticServer:
         try:
             if not self.__es.indices.exists(index_name):
                 # Ignore 400 means to ignore "Index Already Exist" error.
-                res = self.__es.indices.create(index=index_name, ignore=400)
+                res = self.__es.indices.create(index=index_name, ignore=400, body=settings)
                 print("Response:" + str(res))
                 print('Created Index')
                 created = True
@@ -71,41 +74,68 @@ class ElasticServer:
             print(str(ex))
         finally:
             return created
+        
+    # Not used
+    def add_document(self, index_name, doc_type, doc, doc_id=None):
+        resp = self.__es.index(index=index_name, doc_type=doc_type, body=doc, id=doc_id)
+        print(resp)
+        print("Document created")
 
-    def store_records(self):
-        pass
+    def store_records(self, index_name):
+        # later records can be read from a file
+        rec1 = {
+            "id": 1,
+            "Title": "Overview",
+            "Keywords": "Hello, this, is, a, test.",
+            "Paragraph": "Hello this is a test and a test this is."
+        }
+        rec2 = {
+            "id": 2,
+            "Title": "Intro",
+            "Keywords": "Hello, this, is, a, test.",
+            "Paragraph": "Hello this is a test and a test this is."
+        }
+        rec3 = {
+            "id": 3,
+            "Title": "Topic 1",
+            "Keywords": "Hello, this, is, a, test.",
+            "Paragraph": "Hello this is a test and a test this is."
+        }
+        try:
+            outcome = self.__es.index(index=index_name, body=rec1)
+        except Exception as ex:
+            print('Error in indexing data')
+            print(str(ex))
+        try:
+            outcome = self.__es.index(index=index_name, body=rec2)
+        except Exception as ex:
+            print('Error in indexing data')
+            print(str(ex))
+        try:
+            outcome = self.__es.index(index=index_name, body=rec3)
+        except Exception as ex:
+            print('Error in indexing data')
+            print(str(ex))
 
-    def get_shard(self):
-        pass
+    def get_shard(self, index_name ):
+        # es = connect_elasticsearch()
+        if self.__es is not None:
+            search_object = json.dumps({
+                "query": {
+                    "match": {
+                        "Title": "Overview"
+                    }
+                }
+            })
+            resp = self.__es.search(index=index_name, body=search_object)
+        print("Record:")
+        print(resp)
+        # Returning a response but it does not contain the details of the record.
 
 
 
-
-"""e1={
-    "first_name":"nitin",
-    "last_name":"panwar",
-    "age": 27,
-    "about": "Love to play cricket",
-    "interests": ['sports','music'],
-}
-e2={
-    "first_name" :  "Jane",
-    "last_name" :   "Smith",
-    "age" :         32,
-    "about" :       "I like to collect rock albums",
-    "interests":  [ "music" ]
-}
-e3={
-    "first_name" :  "Douglas",
-    "last_name" :   "Fir",
-    "age" :         35,
-    "about":        "I like to build cabinets",
-    "interests":  [ "forestry" ]
-}
-res = es.index(index='megacorp',doc_type='employee',id=1,body=e1)
+"""res = es.index(index='megacorp',doc_type='employee',id=1,body=e1)
 res = es.index(index='megacorp',doc_type='employee',id=2,body=e2)
 res = es.index(index='megacorp',doc_type='employee',id=3,body=e3)
 res=es.get(index='megacorp',doc_type='employee',id=3)
 print (res['_source'])"""
-
-
