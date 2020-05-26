19f5abff85b86f84d963eab9ad027e343164e013
Srinath Ravikumar <srinathlrk@gmail.com>
2020-05-26 22:16:42 +0530

    Adding code to get selected_titles into QueryProcessor class

--- Processor/QuesDataToElasticSearch.py
+++ Processor/QuesDataToElasticSearch.py
@@ -5,25 +5,18 @@ Created on Fri Mar 13 19:35:04 2020
 @author: Srinath Ravikumar
 """
 
-# Decomission this - We dont need an explicit class to convert data to RAKE. Also ElasticSearch Feeder was supposed to be done by Shubham
-
 from Database.Mongo import Mongo
-
+from ElasticServer import ElasticServer
 
 class QuesDataToElasticSearch():
 
     def __init__(self):
         pass
 
-    def QuesDataToElasticSearch(self, raked_data, query_ranked_phrase_with_score):
-        selected_rake_para = 0
-
-        return selected_rake_para
-
-    def matchRakedParaToTextPara(self, selected_raked_para):
-        selected_text_para = db.getFrom("data", "processed_data", """ filter: condition for matching """)
-
-        return selected_text_para
+    def QuesDataToElasticSearch(self, query):
+        es = ElasticServer()
+        selected_titles = es.get_shard("esindex", query)
+        print("selected_titles : ", selected_titles)
+        return selected_titles
 
 
-db = Mongo()
